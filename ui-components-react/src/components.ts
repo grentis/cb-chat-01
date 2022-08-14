5e453e8e72aaf64e8cc66e1bd1a30229371bd4ce
grentis <grentis@gmail.com>
2022-08-15 00:38:28 +0200

    ui-components-react

---
+++ ui-components-react/src/components.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'ui-components-stencil';
+
+
+
+export const UiButton = /*@__PURE__*/createReactComponent<JSX.UiButton, HTMLUiButtonElement>('ui-button');
---
+++ ui-components-react/src/index.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'ui-components-stencil';
+
+import { defineCustomElements } from 'ui-components-stencil/loader';
+
+defineCustomElements();
+export const UiButton = /*@__PURE__*/createReactComponent<JSX.UiButton, HTMLUiButtonElement>('ui-button');
+export const UiInput = /*@__PURE__*/createReactComponent<JSX.UiInput, HTMLUiInputElement>('ui-input');
+export const UiLoading = /*@__PURE__*/createReactComponent<JSX.UiLoading, HTMLUiLoadingElement>('ui-loading');
+export const UiLogin = /*@__PURE__*/createReactComponent<JSX.UiLogin, HTMLUiLoginElement>('ui-login');
+export const UiMessageitem = /*@__PURE__*/createReactComponent<JSX.UiMessageitem, HTMLUiMessageitemElement>('ui-messageitem');
+export const UiMessagelist = /*@__PURE__*/createReactComponent<JSX.UiMessagelist, HTMLUiMessagelistElement>('ui-messagelist');
+export const UiPanel = /*@__PURE__*/createReactComponent<JSX.UiPanel, HTMLUiPanelElement>('ui-panel');
+export const UiRow = /*@__PURE__*/createReactComponent<JSX.UiRow, HTMLUiRowElement>('ui-row');
+export const UiUseritem = /*@__PURE__*/createReactComponent<JSX.UiUseritem, HTMLUiUseritemElement>('ui-useritem');
+export const UiUserlist = /*@__PURE__*/createReactComponent<JSX.UiUserlist, HTMLUiUserlistElement>('ui-userlist');
